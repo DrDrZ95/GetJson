7a8b81429449fbcd9456075e5e65453c9d473170
DrDrZ95 <83525786@qq.com>
2019-07-06 15:10:52 +0800

    show the result

--- Azure-Speech/Program.cs
+++ Azure-Speech/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,20 +12,17 @@ namespace Azure_Speech
 {
     class Program
     {
-        public static string requestUri = "https://westus.stt.speech.microsoft.com/"; //speech/recognition/conversation/cognitiveservices/v1
+        public static string requestUri = "https://westus.stt.speech.microsoft.com/speech/recognition/conversation/cognitiveservices/v1"; //speech/recognition/conversation/cognitiveservices/v1
 
-        public static string audioFile = null;
+        public static string audioFile = @"C:\Users\zhu\Desktop\人工智能（语音识别）-教学\whatstheweatherlike.wav";
 
-        public static string Key = "8185f795483545288123abcc1f0ba65c";
+        public static string subscriptionKey = "8185f795483545288123abcc1f0ba65c";
 
         static void Main(string[] args)
         {
-            //通过httpClient 和 HttpContent 提交获取JSON 【等同于Postman的操作】
-
-
-
-
-
+            //通过HttpWebRequest或者(httpClient 和 HttpWebRequest) 提交获取JSON 【等同于Postman的操作】
+            
+            
             //将以下JSON获取到 显示在WriteLine()里；
             //{
             //    "RecognitionStatus": "Success",
